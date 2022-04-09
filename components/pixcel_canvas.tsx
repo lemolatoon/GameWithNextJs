a21a77ae6c5e998a9324a0ed0e1041ca867f3833
lemolatoon <khishida55@gmail.com>
2022-04-09 19:34:29 +0900

    Add boarderline to canvas

--- components/pixcel_canvas.tsx
+++ components/pixcel_canvas.tsx
@@ -10,25 +10,41 @@ export const GameCanvas: FunctionComponent = () => {
   const [imageURL, setImageURL] = useState<string | null>(null);
   const [pixcel, setPixcel] = useState<Pixcels>();
   // 24 * 10 array
+  const [context, setContext] = useState<CanvasRenderingContext2D>(null);
+
+  const boardBlockX = 10;
+  const boardBlockY = 24;
 
   const width = 600;
   const height = 600;
 
-  const blockSize: number = 15;
+  const blockSize: number = 20;
+  const marginX = 5;
+  const marginY = 10;
 
-  const boardInfo = {
-    blockSize: blockSize,
-    start_x: 30 as number,
-    start_y: 30 as number,
-    width: (10 + 20) * blockSize,
-    height: (24 + 10) * blockSize,
-  } as const;
+  const startX = 30;
+  const startY = 30;
+  const boardStartX = startX + marginX;
+  const boardStartY = startY + marginY;
+
+  const backgroundWidth = boardBlockX * blockSize + marginX * 2;
+  const backgroundHeight = boardBlockY * blockSize + marginY * 2;
+  const boardWidth = boardBlockX * blockSize;
+  const boardHeight = boardBlockY * blockSize;
+
+  // init
+  useEffect(() => {
+    const canvas: HTMLCanvasElement = document.getElementById(
+      "canvas"
+    ) as HTMLCanvasElement;
+  });
 
   useEffect(() => {
     const canvas = document.createElement("canvas");
     canvas.width = width;
     canvas.height = height;
 
+    // background---
     const baseContext = canvas.getContext("2d");
     if (baseContext != null) {
       baseContext.fillStyle = "#2f4f4f";
@@ -39,35 +55,43 @@ export const GameCanvas: FunctionComponent = () => {
     if (underlyingContext != null) {
       underlyingContext.fillStyle = "gray";
       underlyingContext.fillRect(
-        boardInfo.start_x,
-        boardInfo.start_y,
-        boardInfo.width,
-        boardInfo.height
+        startX,
+        startY,
+        backgroundWidth,
+        backgroundHeight
       );
     }
+    // -------------
 
+    // write line
     const blockContext = canvas.getContext("2d");
     const blockImageData = blockContext.getImageData(
-      0,
-      0,
-      boardInfo.width,
-      boardInfo.height
+      boardStartX,
+      boardStartY,
+      boardWidth,
+      boardHeight
     );
     if (blockContext != null) {
       const pixcel = blockImageData.data;
-      for (let i = 0; i < boardInfo.width; i++) {
-        for (let j = 0; j < boardInfo.height; j++) {
+      for (let i = 0; i < boardWidth; i++) {
+        for (let j = 0; j < boardHeight; j++) {
           for (let k = 0; k < 4; k++) {
-            pixcel[(i * boardInfo.height + j) * 4 + k] = PixcelColor.I_COLOR[k];
+            const white = [0xff, 0xff, 0xff, 0xff];
+            const rate = 0.01;
+            const xCondition =
+              Math.abs((i % blockSize) - blockSize) < blockSize * rate ||
+              Math.abs((i % blockSize) - blockSize) > blockSize * (1 - rate);
+            const yCondition =
+              Math.abs((j % blockSize) - blockSize) < blockSize * rate ||
+              Math.abs((j % blockSize) - blockSize) > blockSize * (1 - rate);
+            if (xCondition || yCondition) {
+              pixcel[(i * boardHeight + j) * 4 + k] = white[k];
+            }
           }
         }
       }
     }
-    blockContext.putImageData(
-      blockImageData,
-      boardInfo.start_x,
-      boardInfo.start_y
-    );
+    blockContext.putImageData(blockImageData, boardStartX, boardStartY);
 
     setImageURL(canvas.toDataURL("image/png"));
   });
